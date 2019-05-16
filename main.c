9eea35d6ed441a8883257822b1b8ed2a43dda095
Spiritfaer <30905488+Spiritfaer@users.noreply.github.com>
2019-05-16 10:08:59 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int32_t	backpack(int32_t sum, int32_t const *w, int32_t size)
+{
+	int32_t temp, i, j, answer;
+	answer = temp = 0;
+	j = size;
+	while (temp != sum && j >= 0)
+	{
+		i = 0;
+		temp = w[j];
+		while (temp < sum)
+		{
+			if ((temp + w[i]) < sum)
+				temp += w[i++];
+			else
+				break;
+		}
+		if (temp > answer && temp < sum)
+			answer = temp;
+		j--;
+	}
+	return (answer);
+}
+
+int main(void)
+{
+	int32_t sum = 18;
+	int32_t weights[] = { 3, 7, 10, 16 };
+
+	printf("Answer = %d\n", backpack(sum, weights, sizeof(weights) / sizeof(int32_t) - 1));
+	return (0);
+}
